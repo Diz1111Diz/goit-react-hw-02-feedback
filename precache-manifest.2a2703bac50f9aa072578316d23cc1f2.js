79b28b48a6e1d4eb4dfad4ea7868556b3758818b
unknown <kavunv@gmail.com>
2020-04-11 21:33:34 +0300

    Updates

---
+++ precache-manifest.2a2703bac50f9aa072578316d23cc1f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e275d6c6c9bc974d933b2e77ff6c07",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "c4a01fe4b1cc1395e9ac",
+    "url": "/goit-react-hw-02-feedback/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "aedb2cb88f62dda6a10c",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0edf2d9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0edf2d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a01fe4b1cc1395e9ac",
+    "url": "/goit-react-hw-02-feedback/static/js/main.d9e521fa.chunk.js"
+  },
+  {
+    "revision": "1a2af30d5f9b5771679f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.a031b346.js"
+  }
+]);
